911929152fec117a2505acda2ff688f09045ab20
675naturalcommon <653300386@qq.com>
2026-02-15 21:33:50 +0800

    new

--- Study/C进阶/自定义类型/自定义类型/struct.c
+++ Study/C进阶/自定义类型/自定义类型/struct.c
@@ -3,73 +3,70 @@
 #include <stdio.h>
 #include <string.h>
 
-//结构体类型的定义
+// 结构体类型的定义
 
-//学生
+// 学生
 
-//struct Stu
+// struct Stu
 //{
 //	//学生的相关属性
 //	char name[20];
 //	int age;
-//};
+// };
 //
-//struct Stu
+// struct Stu
 //{
 //	//学生的相关属性
 //	char name[20];
 //	int age;
-//}s1, s2;//s1和s2是struct Stu类型的变量,且是全局变量
+// }s1, s2;//s1和s2是struct Stu类型的变量,且是全局变量
 //
 ////匿名结构体类型
 ////只能用1次
-//struct
+// struct
 //{
 //	int a;
 //	float x;
-//} s1;
+// } s1;
 //
-//int main()
+// int main()
 //{
 //	struct Stu s3;  //局部变量
 //	return 0;
-//}
+// }
 
+// 结构体内存对齐
 
-//结构体内存对齐
-
-//struct  S1
+// struct  S1
 //{
 //	char c1;
 //	int i;
 //	char c2;
-//};
+// };
 //
-//struct  S2
+// struct  S2
 //{
 //	char c1;
 //	char c2;
 //	int i;
-//};
+// };
 //
-//int main()
+// int main()
 //{
 //	printf("%d\n", sizeof(struct S1));//12
 //	printf("%d\n", sizeof(struct S2));//8
 //
 //	return 0;
-//}
+// }
 
-
-
-//struct S
+// struct S
 //{
 //	int data[100];
 //	int num;
-//};
+// };
 //
 //
-//void print1(struct S ss)
+// void print1(struct S ss)
 //{
 //	int i = 0;
 //	for (i = 0; i < 3; i++)
@@ -77,9 +74,9 @@
 //		printf("%d ", ss.data[i]);
 //	}
 //	printf("%d\n", ss.num);
-//}
+// }
 //
-//void print2(const struct S* ss)
+// void print2(const struct S* ss)
 //{
 //	int i = 0;
 //	for (i = 0; i < 3; i++)
@@ -87,28 +84,27 @@
 //		printf("%d ", ss->data[i]);
 //	}
 //	printf("%d\n", ss->num);
-//}
+// }
 //
 //
-//int main()
+// int main()
 //{
 //	struct S s = { {1,2,3},100 };
 //	print1(s);    //传值调用
 //	print2(&s);   //传址调用
 //	return 0;
-//}
- 
+// }
 
-struct A
-{
-	int _a : 2;
-	int _b : 5;
-	int _c : 10;
-	int _d : 20;
-};
+// struct A
+// {
+// 	int _a : 2;
+// 	int _b : 5;
+// 	int _c : 10;
+// 	int _d : 20;
+// };
 
-int main()
-{
-	printf("%d", sizeof(struct A));
-	return 0;
-}
+// int main()
+// {
+// 	printf("%d", sizeof(struct A));
+// 	return 0;
+// }
